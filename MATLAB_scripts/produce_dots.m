8bf5c967e3a3787cf817508584092dc7ae1b6f9d
Adrian Ernesto Radillo <adrian.radillo@gmail.com>
2019-01-02 16:09:53 -0500

    prepare for seed check -- tmp commit

--- MATLAB_scripts/produce_dots.m
+++ MATLAB_scripts/produce_dots.m
@@ -2,6 +2,41 @@
 
 clear all
 
+
+data_folder = '/Users/adrian/Documents/MATLAB/projects/dotsStimExperiments/data/';
+fileName = 'detail_9';
+fffs = {'tomerge_1','tomerge_2'};
+
+%fileToLoad = [fileName,'.mat'];
+
+% column names in final csv file
+colNames={...
+    'stencilNumber', ...
+    'pixelSize', ...
+    'diameter', ...
+    'speed', ...
+    'yCenter', ...
+    'xCenter', ...
+    'density', ...
+    'direction', ...
+    'coherence', ...
+    'dotsDuration', ...
+    'randSeedBase', ...
+    'coherenceSTD', ...
+    'frameIdx', ...
+    'onsetTime', ...
+    'onsetFrame', ...
+    'swapTime', ...
+    'isTight', ...
+    'iter', ...
+    'dotIdx', ...
+    'xpos', ...
+    'ypos'};
+
+numCols=length(colNames);
+numIter = 2;
+
+
 %% set up a dotsDrawable object
 
 % get specific parameters to draw the dots:
@@ -24,69 +59,79 @@ dotsParams.dotsDuration = .5; % in sec
 dotsParams.randSeedBase = 1;
 dotsParams.coherenceSTD = 0; % I don't know what that is
 
+% vector to write to csv files
+paramVec = [...
+    dotsParams.stencilNumber, ...
+    dotsParams.pixelSize, ...
+    dotsParams.diameter, ...
+    dotsParams.speed, ...
+    dotsParams.yCenter, ...
+    dotsParams.xCenter, ...
+    dotsParams.density, ...
+    dotsParams.direction, ...
+    dotsParams.coherence, ...
+    dotsParams.dotsDuration * 1000, ... % in msec
+    dotsParams.randSeedBase, ...
+    dotsParams.coherenceSTD];
+
 displayIndex = 1;
 
+
+
+
+
+
 %% Draw the dots
-info_frames=draw_dots(dotsParams, displayIndex);
-
-
+for jjj = 1:numIter
+    info_frames=draw_dots(dotsParams, displayIndex);
+    
+    
 %% Dump all stimulus data that will be used by our data analysis in R
-
-%clear all
-data_folder = '/Users/adrian/Documents/MATLAB/projects/dotsStimExperiments/data/';
-fileName = 'detail_8';
-save([data_folder,fileName,'.mat'])
-%fileToLoad = [fileName,'.mat'];
-fileToWrite = [data_folder,fileName,'.csv'];
-
-%load(fileToLoad)
-
-% count number of frames actually drawn
-numFrames=count_frames(info_frames);
-
-% column names in final csv file
-colNames={'frameIdx', ...
-          'onsetTime', ...
-          'onsetFrame', ...
-          'swapTime', ...
-          'isTight', ...
-          'dotIdx', ...
-          'xpos', ...
-          'ypos'};
-
-numCols=length(colNames);
-
-% build matrix that will be dumped as a csv file
-
-bigNumber = 10000 * numFrames; % overestimate of the number of dots
-dataMatrix = zeros(bigNumber,numCols);
-
-matrixRow = 1;
-
-for frameIdx = 1:numFrames
-    currFrame = info_frames{frameIdx};
     
-    % 2-by-numDots matrix for current frame
-    dotsMatrix = currFrame.dotsFrameMatrix;
     
-    numRowsForThisFrame = size(dotsMatrix,2);
     
-    % prepare all but last 3 cols of the 'standard row' to fill
-    standardRow = [frameIdx, currFrame.onsetTime, currFrame.onsetFrame,...
-        currFrame.swapTime, currFrame.isTight];
+    %load(fileToLoad)
     
-    for dotIdx = 1:numRowsForThisFrame
-        varyingRow = [dotIdx, dotsMatrix(:,dotIdx)'];
-        dataMatrix(matrixRow,:) = [standardRow, varyingRow];
-        matrixRow = matrixRow + 1;
+    % count number of frames actually drawn
+    numFrames=count_frames(info_frames);
+    
+    
+    % build matrix that will be dumped as a csv file
+    
+    bigNumber = 10000 * numFrames; % overestimate of the number of dots
+    dataMatrix = zeros(bigNumber,numCols);
+    
+    
+    
+    
+    matrixRow = 1;
+    
+    for frameIdx = 1:numFrames
+        currFrame = info_frames{frameIdx};
+        
+        % 2-by-numDots matrix for current frame
+        dotsMatrix = currFrame.dotsFrameMatrix;
+        
+        numRowsForThisFrame = size(dotsMatrix,2);
+        
+        % prepare all but last 3 cols of the 'standard row' to fill
+        standardRow = [paramVec, frameIdx, currFrame.onsetTime, currFrame.onsetFrame,...
+            currFrame.swapTime, currFrame.isTight, jjj];
+        
+        for dotIdx = 1:numRowsForThisFrame
+            varyingRow = [dotIdx, dotsMatrix(:,dotIdx)'];
+            dataMatrix(matrixRow,:) = [standardRow, varyingRow];
+            matrixRow = matrixRow + 1;
+        end
     end
+    
+    dataMatrix = dataMatrix(1:matrixRow-1, :);
+    
+    T=array2table(dataMatrix, 'VariableNames', colNames);
+    %save([data_folder,fileName,'.mat']) % to save workspace
+    fileToWrite = [data_folder, fileName, fffs{jjj}, '.csv'];
+    writetable(T,fileToWrite,'WriteRowNames',true)  % files will need to be merged later
 end
-
-dataMatrix = dataMatrix(1:matrixRow-1, :);
-
-T=array2table(dataMatrix, 'VariableNames', colNames);
-writetable(T,fileToWrite,'WriteRowNames',true) 
-
 
 
 %% Construct stimulus matrix to feed the ME calculator from George Mather
