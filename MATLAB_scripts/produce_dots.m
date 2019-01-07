c578b1a52f5894eb7d320ccf917e7d09b0633c95
Adrian Ernesto Radillo <adrian.radillo@gmail.com>
2019-01-07 15:27:05 -0500

    detail_13 desktop

--- MATLAB_scripts/produce_dots.m
+++ MATLAB_scripts/produce_dots.m
@@ -4,7 +4,7 @@ clear all
 
 
 data_folder = '/Users/adrian/Documents/MATLAB/projects/dotsStimExperiments/data/';
-fileName = 'detail_10';
+fileName = 'detail_13';
 fffs = {'tomerge_1','tomerge_2'};
 
 %fileToLoad = [fileName,'.mat'];
@@ -35,7 +35,7 @@ colNames={...
     'isCoherent'};
 
 numCols=length(colNames);
-numIter = 2;
+numIter = 1;
 
 
 %% set up a dotsDrawable object
@@ -53,9 +53,9 @@ dotsParams.diameter = 10;
 dotsParams.speed = 2;
 dotsParams.yCenter = 0;
 dotsParams.xCenter = 0;
-dotsParams.density = 3;
+dotsParams.density = 1;
 dotsParams.direction = 0;
-dotsParams.coherence = 70;
+dotsParams.coherence = 90;
 dotsParams.dotsDuration = .5; % in sec
 dotsParams.randSeedBase = 1;
 dotsParams.coherenceSTD = 0; % I don't know what that is
@@ -123,40 +123,13 @@ for jjj = 1:numIter
     dataMatrix = dataMatrix(1:matrixRow-1, :);
     
     T=array2table(dataMatrix, 'VariableNames', colNames);
-    %save([data_folder,fileName,'.mat']) % to save workspace
+    save([data_folder,fileName,'.mat']) % to save workspace
     fileToWrite = [data_folder, fileName, fffs{jjj}, '.csv'];
     writetable(T,fileToWrite,'WriteRowNames',true)  % files will need to be merged later
 end
 
 
-%% Construct stimulus matrix to feed the ME calculator from George Mather
-% count number of frames actually drawn
-% numFrames=count_frames(info_frames);
-% 
-% % boolean needed because of the structure of the ME calculation
-% if ~mod(numFrames,2) % true if numFrames is even
-%     addTrivialFrame=true;
-%     length_t = numFrames+1;
-% else
-%     addTrivialFrame=false;
-%     length_t = numFrames;
-% end
-% 
-% % loop over frames and construct both a t-x and a xyt matrix
-% length_x=501; % size of x dimension (MUST BE ODD)
-% txMatrix=zeros(length_t,length_x);
-% xytMatrix = zeros(length_x,length_x, length_t); % x-y-t
-% for ii=1:numFrames
-%     dotsMatrix = inspect_dotsFrameMatrix(info_frames, ii, length_x, false);
-%     xytMatrix(:,:,ii) = dotsMatrix>0;
-%     txMatrix(ii,:) = sum(dotsMatrix,1)>0; % project all dots onto x-axis
-% end
-% 
-% if addTrivialFrame
-%     txMatrix=[txMatrix; zeros(1,length_x)];
-% end
-% 
-% MotionEnergy_1(txMatrix, (length_x-1)/2, (length_t-1)/2);
+
 
 %% Redraw the dots, out of the 3D matrix gathered by snow-dots
 % inspired from this post
